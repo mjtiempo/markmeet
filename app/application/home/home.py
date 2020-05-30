6dc25f350724a44ece8e883406bf945c45f86a8c
Mark <markjohn.tiempo@gmail.com>
2020-05-31 06:48:16 +0800

    refactor templates' layout

--- app/application/home/home.py
+++ app/application/home/home.py
@@ -3,11 +3,9 @@ from flask import current_app as app
 
 
 #Configure Blueprint
-home_bp = Blueprint('home_bp', __name__, 
-                    template_folder='templates',
-                    static_folder='static')
+home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/', methods=['GET'])
-def home():
+def index():
     #homepage
-    return render_template('home.html', title="Flask Home")
+    return render_template('home/home.html', title="Flask Home")
